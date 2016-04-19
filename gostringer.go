923318ba223f5a57eb6baaf11802add5315be67c
Dmitri Shuralyov <shurcooL@gmail.com>
2016-04-18 23:26:13 -0700

    Fix usage line typo.
    Apply change in golang/tools@321c6fd1712ce3e7b050d41b524e6ec44f945f15.

--- gostringer.go
+++ gostringer.go
@@ -90,7 +90,7 @@ var (
 func Usage() {
 	fmt.Fprintf(os.Stderr, "Usage of %s:\n", os.Args[0])
 	fmt.Fprintf(os.Stderr, "\tgostringer [flags] -type T [directory]\n")
-	fmt.Fprintf(os.Stderr, "\tgostringer [flags[ -type T files... # Must be a single package\n")
+	fmt.Fprintf(os.Stderr, "\tgostringer [flags] -type T files... # Must be a single package\n")
 	fmt.Fprintf(os.Stderr, "For more information, see:\n")
 	fmt.Fprintf(os.Stderr, "\thttps://godoc.org/github.com/sourcegraph/gostringer\n")
 	fmt.Fprintf(os.Stderr, "Flags:\n")
